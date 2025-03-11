8886efa6373b91b9f17ebf39c8e117fb1e416669
Ana Lopes <carol.llopes@hotmail.com>
2025-03-11 19:19:29 -0300

    Add Tailwind CSS and create Banner component with styling

---
+++ src/Componentes/Banner/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return ( 
+    <div className="banner">
+	  <h1>👾 CodeLingo</h1>
+	  <p>Aprenda a programar de forma divertida!</p>
+	</div>
+  );
+}
+
+export default Banner;
+
